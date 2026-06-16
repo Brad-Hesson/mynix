2cb73f7550440ed3fd97ca6d1e367e8ae365f056
Brad Hesson <brad.hesson@outlook.com>
2026-06-16 14:49:45 -0600

    initial

---
+++ flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    brad-utils.url = "github:Brad-Hesson/brad-utils";
+    wgsl-analyzer = {
+      url = "github:wgsl-analyzer/wgsl-analyzer";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.crane.follows = "crane";
+    };
+  };
+  outputs = flakes: flakes.flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import flakes.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      brad-utils = flakes.brad-utils.mkLib pkgs;
+      fenix = flakes.fenix.packages.${system};
+      crane = (flakes.crane.mkLib pkgs).overrideToolchain (fenix.combine [
+        fenix.stable.defaultToolchain
+        fenix.stable.rust-src
+      ]);
+      crateArgs = {
+        src = ./.;
+        strictDeps = true;
+      };
+      cargoArtifacts = crane.buildDepsOnly crateArgs;
+      crate = crane.buildPackage (crateArgs // {
+        inherit cargoArtifacts;
+        doCheck = false;
+      });
+    in
+    {
+      packages.default = crate;
+      devShell = crane.devShell {
+        inputsFrom = [ crate ];
+        shellHook = ''
+          ${brad-utils.vscodeSettingsHook {"wgsl-analyzer.server.path" =  "${wgsl-analyzer}/bin/wgsl-analyzer";}}
+        '';
+      };
+    }
+  );
+}
+
