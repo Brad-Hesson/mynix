98c154b15eaf0f7a256150357228f3332772b28a
Brad Hesson <brad.hesson@outlook.com>
2026-06-23 23:11:14 -0600

    changed release profile

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,18 @@ use crate::lexer::file_p;
 mod lexer;
 
 fn main() {
-    let text = std::fs::read_to_string("./nix_lexer_edge_cases.nix").unwrap();
-    // let text = std::fs::read_to_string("./flake.nix").unwrap();
+    // let text = std::fs::read_to_string("./nix_lexer_edge_cases.nix").unwrap();
+    let text = std::fs::read_to_string("./flake.nix").unwrap();
     let src = text.as_str();
     // while let Ok(tok) = token_p::<_, InputError<_>>(&mut src) {
     let mut p = file_p::<_, ErrMode<EmptyError>>;
     let mut num = 0;
-    for _ in 0..2u64.pow(10) {
+    for _ in 0..2u64.pow(12) {
         for tok in p.parse(src).unwrap() {
             num += 1;
-            print!("{} ", tok);
+            // print!("{} ", tok);
         }
-        break;
+        // break;
     }
     println!("{num}");
 }
